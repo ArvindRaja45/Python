2ac228ed2ed1fcecaeb6f14d9107bf0cbf1c3fd2
Arvind Raja N <158467220+ArvindRaja45@users.noreply.github.com>
2025-02-04 15:38:08 +0530

    Project Day1.py

--- Day1.py
+++ Day1.py
@@ -1,2 +1,10 @@
-# Use what you learnt to print out the words "Hello world!" with Python code.
-print("Hello world!")
+#BANK NAME GENERATOR PROJECT
+#Create a greeting for your program.
+#Ask the user for the city that they grew up in and store it in a variable.
+#Ask the user for the name of a pet and store it in a variable.
+#Combine the name of their city and pet and show them their band name.
+
+print("Welcome to great Bank Name Generator Program")
+a = input("Which city did you grow up in?\n")
+b = input("What is the name of the pet\n")
+print("Your band name should be" + a + " " + b )
